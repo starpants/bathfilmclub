37f81e03b36e6e6fb35cb1d7be9cd8c259d61dec
starpants <av.bains@gmail.com>
2026-06-24 09:32:47 +0100

    favicon add motif mixed back

--- site/src/components/FilmStrip.tsx
+++ site/src/components/FilmStrip.tsx
@@ -1,8 +1,8 @@
 export function FilmStrip() {
   return (
-    <div className="flex justify-center gap-6 pt-10">
+    <div className="flex justify-center gap-8 pt-10">
       {[0, 1, 2, 3, 4, 5, 6, 7].map((i) => (
-        <div key={i} className="w-3 h-3 bg-bfc-brand-fg" />
+        <div key={i} className="w-4 h-4 bg-bfc-brand-fg/60" />
       ))}
     </div>
   );
